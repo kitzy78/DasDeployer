819db9c3676955979bf3eb6fde014cad5bcd314e
Tom Kitzmiller <kitzy78@yahoo.com>
2021-08-29 15:47:09 -0400

    Create irelease.py

---
+++ dasdeployer/irelease.py
@@ -0,0 +1,19 @@
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+from gpiozero import LEDBoard, ButtonBoard, Button
+import pprint
+
+# Fill in with your personal access token and org URL
+personal_access_token = 'ilykq7ypqmledu64xxrthio6cja4anmtiihx4iobf54xqprpdlga'
+organization_url = 'https://dev.azure.com/revlocal'
+projectDefinitionId = 140
+
+# Create a connection to the org
+credentials = BasicAuthentication('', personal_access_token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+release_client = connection.clients.get_release_client()
+#releaseStartMetadata = ReleaseStartMetadata(definition_id=projectDefinitionId)
+#print(releaseStartMetadata)
+projectDefinition = release_client.get_release_definition(project='GitRepos', definition_id=projectDefinitionId)
+print(projectDefinition)
