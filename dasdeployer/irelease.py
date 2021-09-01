2eddeb24227f318a632808060025fe666b705ef0
Tom Kitzmiller <kitzy78@yahoo.com>
2021-08-31 20:14:30 -0400

    Added buttons and lights

--- dasdeployer/irelease.py
+++ dasdeployer/irelease.py
@@ -1,25 +1,49 @@
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
-from gpiozero import LEDBoard, ButtonBoard, Button
+from azure.devops.v5_1.release.models import BuildVersion, ArtifactMetadata, ReleaseStartMetadata
+from gpiozero import LED, Button
 import pprint
 
 # Fill in with your personal access token and org URL
-personal_access_token = 'nope'
+personal_access_token = 'ilykq7ypqmledu64xxrthio6cja4anmtiihx4iobf54xqprpdlga'
 organization_url = 'https://dev.azure.com/revlocal'
+projectName = 'GitRepos'
 projectDefinitionId = 140
 
-# Create a connection to the org
-credentials = BasicAuthentication('', personal_access_token)
-connection = Connection(base_url=organization_url, creds=credentials)
+# Assign pins to our LEDS
+greenLed = LED(4)
+blueLed = LED(17)
+button = Button(2)
 
-release_client = connection.clients.get_release_client()
-#releaseStartMetadata = ReleaseStartMetadata(definition_id=projectDefinitionId)
-#print(releaseStartMetadata)
-projectDefinition = release_client.get_release_definition(project='GitRepos', definition_id=projectDefinitionId)
-print(projectDefinition)
+# Device is on so we turn green
+greenLed.on()
 
-statusLed = LEDBoard(red=4, pwm=True)
-statusLed.on()
-sleep(3)
-statusLed.off()
+def createRelease():
+    print("Button was pressed")
 
+    # Device is running release so we turn blue
+    greenLed.off()
+    blueLed.blink()
+
+    # Create a connection to the org
+    print('Create connection...')
+    credentials = BasicAuthentication('', personal_access_token)
+    connection = Connection(base_url=organization_url, creds=credentials)
+
+    # Instantiate  release client
+    release_client = connection.clients.get_release_client()
+
+    #projectDefinition = release_client.get_release_definition(project=projectName, definition_id=projectDefinitionId)
+    #print(projectDefinition)
+
+    # Create base metadata with project id of release
+    rsm = ReleaseStartMetadata(definition_id=projectDefinitionId, is_draft=False, artifacts=None)
+    print('Create Release...')
+    response = release_client.create_release(release_start_metadata=rsm, project=projectName)
+    print(response)
+
+    # Device triggered build now we wait
+    blueLed.off()
+    greenLed.on()
+
+button.when_pressed = createRelease
